9d882591aa5a13aa420281398f1817aff2661842
Numaan Baig <71689827+ShoaibNumaan@users.noreply.github.com>
2021-10-26 22:21:12 +0530

    added nav bar and home component templates (#3)

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Shoaib',
+        'title': 'BP 1',
+        'content': "First post in jhool",
+        'date_posted': 'October 26, 2021'
+    },
+    {
+        'author': 'Numaan',
+        'title': 'BP 2',
+        'content': "Second post in jhool",
+        'date_posted': 'October 27, 2021'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', { 'title': 'About'})
