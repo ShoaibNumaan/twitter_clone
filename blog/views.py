ecb9f53524ff07ced5ea7e5db0733c4777330c22
Numaan <shoaib.numaan@gmail.com>
2022-01-04 20:38:50 +0530

    Uploaded the Post model to the admin site

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Shoaib',
-        'title': 'BP 1',
-        'content': "First post in jhool",
-        'date_posted': 'October 26, 2021'
-    },
-    {
-        'author': 'Numaan',
-        'title': 'BP 2',
-        'content': "Second post in jhool",
-        'date_posted': 'October 27, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
